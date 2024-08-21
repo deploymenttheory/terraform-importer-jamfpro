3efaccb6936f17b1af20253d5d6de3d731f162af
Joseph Little <joelittle11@live.com>
2024-08-21 14:48:35 +0100

    Example added

--- python/main.py
+++ python/main.py
@@ -7,7 +7,6 @@ ENV_KEY_TENANT_NAME = "PRO_TENANT_ID"
 ENV_KEY_CLIENT_ID = "CLIENT_ID"
 ENV_KEY_CLIENT_SECRET = "CLIENT_SECRET"
 
-
 CLIENT = jamfpy.init_client(
     tenant_name=os.environ.get(ENV_KEY_TENANT_NAME),
     client_id=os.environ.get(ENV_KEY_CLIENT_ID),
@@ -15,12 +14,5 @@ CLIENT = jamfpy.init_client(
     token_exp_threshold_mins=5
 )
 
-script_options = jamftf.Options(use_resource_type_as_name=False)
-
-importer = jamftf.Importer(CLIENT, targetted=[
-    jamftf.Scripts(options=script_options),
-    jamftf.Categories()
-])
-
-
-print(importer.HCL())
+target_resources = []
+importer = jamftf.Importer(CLIENT, target_resources)
