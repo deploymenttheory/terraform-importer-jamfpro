df8e29e4770fb4410b724e19dee2a8de965cf30e
Joseph Little <joelittle11@live.com>
2024-08-23 15:52:41 +0100

    testing

--- python/main.py
+++ python/main.py
@@ -1,12 +1,14 @@
 import jamftf
 import jamfpy
 import os
-import json
+
+import jamftf.exceptions
 
 # Env Key
 ENV_KEY_TENANT_NAME = "PRO_TENANT_ID"
 ENV_KEY_CLIENT_ID = "CLIENT_ID"
 ENV_KEY_CLIENT_SECRET = "CLIENT_SECRET"
+
 
 CLIENT = jamfpy.init_client(
     tenant_name=os.environ.get(ENV_KEY_TENANT_NAME),
@@ -15,10 +17,18 @@ CLIENT = jamfpy.init_client(
     token_exp_threshold_mins=5
 )
 
-TEST = {
-    "jamfpro_script"
-}
+exclude = [
+    7761,
+    7759,
+    7723,
+    7725,
+    7724,
+    7760
+]
 
-importer = jamftf.Importer(CLIENT, targetted=[jamftf.Scripts(options=jamftf.Options(ignore_illegal_chars=True)), jamftf.Categories()])
+cwd = "/Users/joseph/github"
+opts = jamftf.Options(exclude_ids=exclude)
+scripts = jamftf.Scripts(validate=True, options=opts)
+importer = jamftf.Importer(CLIENT, targetted=[scripts])
+print(importer.HCL(pretty=False, cwd=cwd))
 
-print(importer.HCL())
