c3dfefe5fa372101348a30c5baf4f7d9ac9e8ecf
Joseph Little <joelittle11@live.com>
2024-09-05 12:57:10 +0100

    added env exmaple and new dirs

--- scripts/import_via_config_file.py
+++ scripts/import_via_config_file.py
@@ -1,6 +1,11 @@
+"""
+Module which relies on ENV vars and config file to programatically generate HCL
+import statements using information gathed from Jamf Pro
+"""
+
+import os
 import jamftf
 import jamfpy
-import os
 
 ENV_KEYS = {
     "tenant_id": "JAMFTF_JPRO_TENANT_ID",
@@ -12,13 +17,18 @@ ENV_KEYS = {
 }
 
 def env_vars() -> dict:
+    """
+    Loops through ENV_KEYS and retrieves values from the env
+
+    Will raise a KeyError if any vars are empty or unset.
+    """
     out = {}
     for j, k in ENV_KEYS.items():
         val = os.environ.get(k)
 
         if val is None or val == "":
-            raise KeyError("missing environment var: %s", k)
-        
+            raise KeyError("missing environment var: %s, %s", j, k)
+
         out[j] = val
 
     return out
@@ -26,10 +36,12 @@ def env_vars() -> dict:
 ENV = env_vars()
 
 def get_client():
-    level = 20
-    if bool(ENV["debug_mode"]):
-        level = 10
-    
+    """
+    Inits a base config jamfpy client for API operations
+    """
+
+    level = 10 if ENV["debug_mode"].lower() == "true" else 30
+
     return jamfpy.init_client(
         tenant_name=ENV["tenant_id"],
         client_id=ENV["client_id"],
@@ -39,19 +51,28 @@ def get_client():
 
 
 def get_hcl() -> dict:
+    """
+    Parses the config file, feeds the targets to the importer. 
+
+    Importer generates the HCL and returns in dict format
+    """
     target_resources = jamftf.parse_config_file(ENV["config_file_path"])
+
+    debug = ENV["debug_mode"].lower() == "true"
 
     importer = jamftf.Importer(
         client=get_client(),
         targetted=target_resources,
-        debug=bool(ENV["debug_mode"])
+        debug=debug
     )
 
     return importer.HCLd()
 
 
 def write_out(hcl_dict: dict) -> None:
-    """something"""
+    """
+    Uses HCL dict to create filenames and populate them with import statemens
+    """
     for k, v in hcl_dict.items():
 
         output_fn = k + ".tf"
@@ -61,9 +82,11 @@ def write_out(hcl_dict: dict) -> None:
             f.write(v)
 
 def main():
+    """
+    gets & writes hcl to files
+    """
     write_out(get_hcl())
+
 
 if __name__ == "__main__":
     main()
-        
-    
