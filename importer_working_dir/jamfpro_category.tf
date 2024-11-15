f52e8fc676cb7d71725aadd9773a6b7ac0b3bc7d
Github Action <action@github.com>
2024-11-15 14:03:46 +0000

    Uploading output files

---
+++ importer_working_dir/jamfpro_category.tf
@@ -0,0 +1,15 @@
+
+import {
+  id = 235
+  to = jamfpro_category.jamfpro_category_0
+}
+
+import {
+  id = 237
+  to = jamfpro_category.jamfpro_category_1
+}
+
+import {
+  id = 236
+  to = jamfpro_category.jamfpro_category_2
+}
