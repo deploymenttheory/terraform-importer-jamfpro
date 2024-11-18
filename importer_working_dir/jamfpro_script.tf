4640a261126051caea47254ba6d0f425b1e4808e
Github Action <action@github.com>
2024-11-18 22:41:17 +0000

    Uploading output files

---
+++ importer_working_dir/jamfpro_script.tf
@@ -0,0 +1,25 @@
+
+import {
+  id = 15
+  to = jamfpro_script.jamfpro_script_0
+}
+
+import {
+  id = 18
+  to = jamfpro_script.jamfpro_script_1
+}
+
+import {
+  id = 17
+  to = jamfpro_script.jamfpro_script_2
+}
+
+import {
+  id = 16
+  to = jamfpro_script.jamfpro_script_3
+}
+
+import {
+  id = 14
+  to = jamfpro_script.jamfpro_script_4
+}
